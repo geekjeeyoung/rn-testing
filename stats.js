2ff36e7cd6f6af2d65832965af972f6fc96f2150
Jeeyoung Han <jeeyoung.han82@gmail.com>
2021-11-30 16:59:11 +0100

    🔥 [TDD] failure -> success -> refactoring 방식으로 테스트 주도 개발 연습

---
+++ stats.js
@@ -0,0 +1,50 @@
+exports.max = (numbers) => Math.max(...numbers);
+
+exports.min = (numbers) => Math.min(...numbers);
+
+// exports.avg = (numbers) =>
+//   numbers.reduce(
+//     (acc, current, index, array) =>
+//       index === array.length - 1
+//         ? (acc + current) / array.length
+//         : acc + current,
+//     0
+//   );
+
+exports.avg = (numbers) =>
+  numbers.reduce(
+    (acc, current, index, { length }) => acc + current / length,
+    0
+  );
+
+exports.sort = (numbers) => numbers.sort((a, b) => a - b);
+
+exports.median = (numbers) => {
+  const { length } = numbers;
+  const middle = Math.floor(length / 2);
+  return length % 2
+    ? numbers[middle]
+    : (numbers[middle - 1] + numbers[middle]) / 2;
+};
+
+exports.mode = (numbers) => {
+  const counts = numbers.reduce(
+    (acc, current) => acc.set(current, acc.get(current) + 1 || 1),
+    new Map()
+  );
+
+  const maxCount = Math.max(...counts.values());
+  const modes = [...counts.keys()].filter(
+    (number) => counts.get(number) === maxCount
+  );
+
+  if (modes.length === numbers.length) {
+    return null;
+  }
+
+  if (modes.length > 1) {
+    return modes;
+  }
+
+  return modes[0];
+};
